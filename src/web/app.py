a5bb862ad4439127bfe20d4d85899af3c7680524
Oliver Newland <onewland@gmail.com>
2020-04-05 11:48:17 -0700

    [wip] delete old test classes

--- src/web/app.py
+++ src/web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-import game_orchestrator
+import config
 from command_evaluator import CommandEvaluator
 from deuces import Deck
 from game import Game
@@ -8,14 +8,12 @@ from game_service import GameService
 from hand import Hand
 from player import Player
 from player_command import RaiseCommand, FoldCommand, CallCommand, CheckCommand
-import config
 
 app = Flask(__name__)
 config.load_config(app)
 player_names = ["alfred", "betty", "chris", "dee"]
 players = [Player(name, hand=Hand([])) for name in player_names]
 game = Game(players, Deck(), little_ante=5, big_ante=10)
-orchestrator = game_orchestrator.HoldemGameLoop(game, little_ante=5, big_ante=10)
 service = GameService(game=game)
 command_evaluator = CommandEvaluator(service)
 
@@ -25,19 +23,6 @@ service.deal_cards_1_2()
 @app.route("/")
 def hello_world():
     return "Hello, World!"
-
-
-@app.route("/game", methods=["GET"])
-def echo_game_id():
-    return {"game": orchestrator.json_dict()}
-
-
-@app.route("/game/advance", methods=["POST"])
-def advance():
-    if orchestrator.advance_if_possible():
-        return {"game": "continues"}
-    else:
-        return {"game": "over"}
 
 
 @app.route("/v1/game", methods=["GET"])
